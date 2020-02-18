25888df3e5029f6a5f84731e0d4d179979a58a55
Andrés Rodart <35720017+Andresrodart@users.noreply.github.com>
2020-02-18 09:44:58 -0600

    Update ItsAMurder.py

--- ViJudge/ItsAMurder.py
+++ ViJudge/ItsAMurder.py
@@ -1,26 +1,25 @@
 maxList = 10**6 + 10
-fenwick = [0] * maxList
 
 def update(l, delta):
-	while l < maxList:
-		fenwick[l] += delta
-		l = l | (l + 1)
+  while l < maxList:
+    fenwick[l] += delta
+    l = l | (l + 1)
 
 def add(r):
-	res = 0;
-	while r >= 0:
-		res += fenwick[r];
-		r = (r & (r + 1)) - 1
-	return res
+  res = 0;
+  while r >= 0:
+    res += fenwick[r];
+    r = (r & (r + 1)) - 1
+  return res
 
 if __name__ == '__main__':
-	T = int(input())
-	aux = []
-	for _ in range(T):
-		res = 0
-		leng = int(input()) - 1
-		stairs = list(map(int, input().split()))
-		for num in stairs:
-			res += add(num - 1)
-			update(num, num)
-		print(int(res))
+  T = int(input())
+  for _ in range(T):
+    res = 0
+    fenwick = [0] * maxList
+    leng = int(input()) - 1
+    stairs = list(map(int, input().split()))
+    for num in stairs:
+      res += add(num - 1)
+      update(num, num)
+    print(res)
