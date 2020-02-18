316184089acd023a2be78335fd162ed2800d2a0e
Andres Rl <arodartel1601@alumno.ipn.mx>
2020-02-18 02:13:54 -0600

    vJ

--- ViJudge/ItsAMurder.py
+++ ViJudge/ItsAMurder.py
@@ -1,16 +1,26 @@
-numbers = [0] * (10**5)
+maxList = 10**6 + 10
+fenwick = [0] * maxList
 
-def updateNumbers(num):
-	for i in range(num, (10**5)):
-		numbers[i] = numbers[i] + num
+def update(l, delta):
+	while l < maxList:
+		fenwick[l] += delta
+		l = l | (l + 1)
 
-if __name__ == "__main__":
-	res = 0
+def add(r):
+	res = 0;
+	while r >= 0:
+		res += fenwick[r];
+		r = (r & (r + 1)) - 1
+	return res
+
+if __name__ == '__main__':
 	T = int(input())
+	aux = []
 	for _ in range(T):
+		res = 0
 		leng = int(input()) - 1
 		stairs = list(map(int, input().split()))
 		for num in stairs:
-			res = res + numbers[num - 1]
-			updateNumbers(num)
-		print(res)
+			res += add(num - 1)
+			update(num, num)
+		print(int(res))
