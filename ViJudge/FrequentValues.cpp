7cde9351342e1c21245cd75a37b1f48736218055
Andresrodart <arodartel1601@alumno.ipn.mx>
2020-02-27 23:53:29 -0600

    VJ

---
+++ ViJudge/FrequentValues.cpp
@@ -0,0 +1,40 @@
+#include <utility>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <climits>
+#define ll long long 
+using namespace std;
+
+int main(int argc, char const *argv[]){
+	ll tests = 1, n, querys, element, rigt, lef, res, cont = 1, aux, j = 0;
+	cin >> tests;
+	while (tests != 0){
+		n = tests;
+		cin >> querys;
+		vector<pair<ll, ll>> arr;
+        cin >> element;
+        aux = element;
+        arr.push_back(make_pair(j, cont++));
+		for (int i = 1; i < n; i++){
+			cin >> element;
+			if (element != aux){
+                j = i;
+				cont = 1;
+            }
+			arr.push_back(make_pair(j, cont++));
+		}
+		while (querys--){
+			cin >> lef >> rigt;
+			res = LLONG_MIN;
+			while (lef <= rigt){
+				res = max(res, arr[rigt - 1].second - rigt + 2 + arr[rigt - 1].first);
+				rigt = arr[rigt].first - 1;
+			}
+			std::cout << res << std::endl;
+		}
+		cin >> tests;
+	}
+	
+	return 0;
+}
