0b3a7a76f87aaa1feede3603e265760092478b45
Andresrodart <arodartel1601@alumno.ipn.mx>
2020-03-02 20:44:32 -0600

    VJ

--- ViJudge/FrequentValues.cpp
+++ ViJudge/FrequentValues.cpp
@@ -1,43 +1,110 @@
-#include <utility>
 #include <vector>
 #include <iostream>
-#include <algorithm>
 #include <climits>
 #define ll long long 
 using namespace std;
+const unsigned int MAXN = 100000;
+ll log__2[MAXN + 1];
+
+class SparseTable{
+	private:
+		vector<ll> arr;
+		ll length, k, **ST;
+	public:
+	SparseTable(vector<ll> arr, ll length){
+		this -> arr = arr;
+		this -> length = length;
+		this -> k = log__2[MAXN] + 1;
+		this -> ST = (ll **) malloc(MAXN * sizeof(ll *));
+		for (size_t i = 0; i < MAXN; i++)
+			this -> ST[i] = (ll *) malloc((this -> k + 1) * sizeof(ll));
+		build();
+	}
+	void build();
+	int query(int L, int R);
+};
+void SparseTable::build(){
+	for (size_t i = 0; i < this->length; i++)
+		this->ST[i][0] = 1;
+	for(int j = 1; j < this->k; j++)
+		for(int i = 0; i + (1 << j) <= this->length; i++){
+			int a = this->ST[i][j - 1];
+			int b = this->ST[i + (1 << (j - 1))][j - 1];
+			this->ST[i][j] = (this->arr[i + (1 << (j - 1)) - 1] == this->arr[i + (1 << (j - 1))]) ? (a + b):max(a, b);
+		}
+}
+int SparseTable::query(int L, int R){
+	int j = log__2[R - L + 1];
+	return max(this->ST[L][j], this->ST[R - (1 << j) + 1][j]);
+}
 
 int main(int argc, char const *argv[]){
 	ll tests = 1, n, querys, element, rigt, lef, res, cont = 1, aux, j = 0;
-	cin >> tests;
-	while (tests != 0){
-		n = tests;
-		cin >> querys;
-		vector<pair<ll, ll>> arr;
-        cin >> element;
-        aux = element;
-        arr.push_back(make_pair(j, cont++));
-		for (int i = 1; i < n; i++){
-			cin >> element;
-			if (element != aux){
-                j = i;
-				cont = 1;
-				aux = element;
-            }
-			arr.push_back(make_pair(j, cont++));
-		}
-		while (querys--){
-			cin >> lef >> rigt;
-			res = LLONG_MIN;
-			lef--;
-			rigt--;
-			while (lef <= rigt){
-				res = (lef < arr[rigt].first)? max(res, arr[rigt].second):max(res, arr[rigt].second - arr[rigt - 1].second);
-				rigt = arr[rigt].first - 1;
-			}
-			std::cout << res << std::endl;
-		}
-		cin >> tests;
-	}
-	
+	log__2[1] = 0;
+	for (int i = 2; i <= MAXN; i++)
+    	log__2[i] = log__2[i/2] + 1;
+ 	cin >> tests;
+ 	while (tests != 0){
+ 		n = tests;
+ 		cin >> querys;
+ 		vector<ll> arr;
+        for (int i = 0; i < n; i++){
+ 			cin >> element;
+ 			arr.push_back(element);
+ 		}
+		SparseTable ST{arr, n};
+ 		while (querys--){
+ 			cin >> lef >> rigt;
+ 			res = LLONG_MIN;
+ 			std::cout << ST.query(lef - 1, rigt - 1) << std::endl;
+ 		}
+ 		cin >> tests;
+ 	}
 	return 0;
 }
+
+
+
+// #include <utility>
+// #include <vector>
+// #include <iostream>
+// #include <algorithm>
+// #include <climits>
+// #define ll long long 
+// using namespace std;
+
+// int main(int argc, char const *argv[]){
+// 	ll tests = 1, n, querys, element, rigt, lef, res, cont = 1, aux, j = 0;
+// 	cin >> tests;
+// 	while (tests != 0){
+// 		n = tests;
+// 		cin >> querys;
+// 		vector<pair<ll, ll>> arr;
+//         cin >> element;
+//         aux = element;
+//         arr.push_back(make_pair(j, cont++));
+// 		for (int i = 1; i < n; i++){
+// 			cin >> element;
+// 			if (element != aux){
+//                 j = i;
+// 				cont = 1;
+// 				aux = element;
+//             }
+// 			arr.push_back(make_pair(j, cont++));
+// 		}
+// 		while (querys--){
+// 			cin >> lef >> rigt;
+// 			res = LLONG_MIN;
+// 			lef--;
+// 			rigt--;
+// 			while (lef <= rigt){
+// 				res = (lef < arr[rigt].first)? max(res, arr[rigt].second):max(res, arr[rigt].second - arr[rigt - 1].second);
+// 				rigt = arr[rigt].first - 1;
+// 			}
+// 			std::cout << res << std::endl;
+// 		}
+// 		cin >> tests;
+// 	}
+	
+// 	return 0;
+// }
--- ViJudge/FrequentValues.py
+++ ViJudge/FrequentValues.py
@@ -1,40 +1,81 @@
-import math
-MAXN = 10**5 + 1
-log = [0]*(MAXN+1)
-log[1] = 0;
-
+MAXN = 10
+log2 = [0]*(MAXN + 1)
 for i in range(2, MAXN + 1):
-	log[i] = log[int(i/2)] + 1;
+  log2[i] = log2[int(i/2)] + 1
 
-class sparseTable:
-	def __init__(self, F, n = None, k = None):
-		self.n = MAXN if not n else n
-		self.k = int(math.log2(self.n) + 2 if not k else k)
-		self.F = F
-		self.sparseTable = [[0] * self.k for i in range(self.n)] 
-	def generate(self, array, length = None):
-		size = length if length else len(array)
-		for i in range(size):							# Init values
-			self.sparseTable[i][0] = array[i]
-		for j in range(1, self.k + 1):					# Iterate for each range in K
+class SparseTable:
+	def __init__(self, arr, length = None):
+		self.arr = arr
+		self.length = length if length else len(arr)
+		self.k = log2[MAXN] + 1
+		self.ST = [[1] * (self.k + 1) for _ in range(MAXN)]
+		self.build()
+	def build(self, arr = None):
+		#ToDo if arr reset k and length
+		for j in range(1, self.k + 1):
 			i = 0
-			while i + (1 << j) <= self.n:				# while i + 2**j less than n
-				self.sparseTable[i][j] = self.F(self.sparseTable[i][j - 1], self.sparseTable[i + (1 << (j - 1))][j - 1])	# Compute F from both parts of range
+			while(i + (1 << j) <= self.length):
+				a = self.ST[i][j - 1]
+				b = self.ST[i + (1 << (j - 1))][j - 1]
+				self.ST[i][j] = (a + b) if self.arr[i + (1 << (j - 1)) - 1] == self.arr[i + (1 << (j - 1))] else max(a, b) 
 				i += 1
-	def query(self, R, L):
-		j = log[R - L + 1];
-		return min(self.sparseTable[L][j], self.sparseTable[R - (1 << j) + 1][j]);
+	def query(self, L, R):
+		j = log2[R - L + 1]
+		return max(self.ST[L][j], self.ST[R - (1 << j) + 1][j])
 
 if __name__ == '__main__':
-	test, query = map(int, input().split())
+	n, query = map(int, input().split())
 	while True:
-		elements = sparseTable(min) 
-		elements.generate(list(map(int, input().split())), length = test)
+		elements = list(map(int, input().split()))
+		elements = SparseTable(elements, length = n)
+		for i in elements.ST:
+			print(i)
 		while(query != 0):
 			l, r = map(int, input().split())
 			query -= 1
-			print(elements.query(r, l))
+			print(elements.query(l - 1, r - 1))
 		nxt = list(map(int, input().split()))
 		if len(nxt) == 1: break
 		test = nxt[0]
 		query = nxt[1]
+
+# import math
+# MAXN = 10**5 + 1
+# log = [0]*(MAXN+1)
+# log[1] = 0;
+
+# for i in range(2, MAXN + 1):
+# 	log[i] = log[int(i/2)] + 1;
+
+# class sparseTable:
+# 	def __init__(self, F, n = None, k = None):
+# 		self.n = MAXN if not n else n
+# 		self.k = int(math.log2(self.n) + 2 if not k else k)
+# 		self.F = F
+# 		self.sparseTable = [[0] * self.k for i in range(self.n)] 
+# 	def generate(self, array, length = None):
+# 		size = length if length else len(array)
+# 		for i in range(size):							# Init values
+# 			self.sparseTable[i][0] = array[i]
+# 		for j in range(1, self.k + 1):					# Iterate for each range in K
+# 			i = 0
+# 			while i + (1 << j) <= self.n:				# while i + 2**j less than n
+# 				self.sparseTable[i][j] = self.F(self.sparseTable[i][j - 1], self.sparseTable[i + (1 << (j - 1))][j - 1])	# Compute F from both parts of range
+# 				i += 1
+# 	def query(self, R, L):
+# 		j = log[R - L + 1];
+# 		return min(self.sparseTable[L][j], self.sparseTable[R - (1 << j) + 1][j]);
+
+# if __name__ == '__main__':
+# 	test, query = map(int, input().split())
+# 	while True:
+# 		elements = sparseTable(min) 
+# 		elements.generate(list(map(int, input().split())), length = test)
+# 		while(query != 0):
+# 			l, r = map(int, input().split())
+# 			query -= 1
+# 			print(elements.query(r, l))
+# 		nxt = list(map(int, input().split()))
+# 		if len(nxt) == 1: break
+# 		test = nxt[0]
+# 		query = nxt[1]
