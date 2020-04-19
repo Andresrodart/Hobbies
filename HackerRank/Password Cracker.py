04983f621d034e330fc2f49ed48f21c419c4c8d4
Andres Rl <arodartel1601@alumno.ipn.mx>
2020-04-18 23:36:08 -0500

    HR

--- HackerRank/Password Cracker.py
+++ HackerRank/Password Cracker.py
@@ -14,34 +14,30 @@ import sys
 #  1. STRING_ARRAY passwords
 #  2. STRING loginAttempt
 #
-def isInside(smaller, bigger):
-	index = bigger.find(smaller)
-	if index == -1:
-		return([(bigger, False)])
-
-	res, prev, mid, post = [], [], [], []
-	for i in range(index): prev.append(bigger[i])
-	for i in range(index + len(smaller), len(bigger)): post.append(bigger[i])
-	if len(prev) != 0: res.append((''.join(prev), False))
-	res.append((smaller, True))
-	if len(post) != 0: res.append((''.join(post), False))
-	return(res)
-
-def passwordCracker(passwords, loginAttempt):
-	passwords.sort(key = len, reverse = True)
-	tokenizer, res = [(loginAttempt, False)], []
-	for password in passwords:
-		aux = []
-		for token in tokenizer:
-			if not token[1] and len(password) <= len(token[0]): aux.extend(isInside(password, token[0]))
-			else: aux.append(token)
-		tokenizer = [item for item in aux]
-		print(tokenizer)
+def check_alphabet(passwords, loginAttempt):
+	letters = set(loginAttempt)
+	password = set(''.join(passwords))
+	for char in letters:
+		if char not in password: return False
+	return True
 	
-	for token in tokenizer:
-		if not token[1]: return 'WRONG PASSWORD'
-		res.append(token[0] + ' ')
-	return ''.join(res)
+def passwordCracker(passwords, loginAttempt):
+	i, res, backtracking = 0, [], {}
+	if not check_alphabet(passwords, loginAttempt): return 'WRONG PASSWORD'
+	while i < len(loginAttempt):
+		good = False
+		start = 0 if i not in backtracking else backtracking[i]
+		for index in range(start, len(passwords)):
+			if passwords[index] == loginAttempt[i: i + len(passwords[index])]:
+				good = True
+				res.append(passwords[index])
+				backtracking[i] = index + 1
+				i += len(passwords[index])
+				break
+		if not res: return 'WRONG PASSWORD'
+		if not good:
+			i -= len(res.pop())
+	return ' '.join(res)
 
 if __name__ == '__main__':
 	t = int(input().strip())
@@ -50,5 +46,5 @@ if __name__ == '__main__':
 		n = int(input().strip())
 		passwords = input().rstrip().split()
 		loginAttempt = input()
-		result += passwordCracker(passwords, loginAttempt) + ''
+		result += passwordCracker(passwords, loginAttempt) + '\n'
 	print(result)
